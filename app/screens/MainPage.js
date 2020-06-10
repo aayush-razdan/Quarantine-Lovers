94ecca2e65d9204cea44ee2895bcbd07376fb09f
Ayush Pattnaik <53592579+ayushpattnaik@users.noreply.github.com>
2020-06-10 12:46:04 +0530

    added mainpage with header bar

---
+++ app/screens/MainPage.js
@@ -0,0 +1,83 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home screen under Construction</Text>
+    </View>
+  );
+}
+function TimeTable() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Under Construction</Text>
+    </View>
+  );
+}
+
+function ProfileScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>profile under construction</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+const HomeStack=createStackNavigator();
+const ProfileStack=createStackNavigator();
+const TimetableStack=createStackNavigator();
+
+const HomeStackScreen=()=>(
+  <HomeStack.Navigator>
+  <HomeStack.Screen name="Home" component={HomeScreen}/>
+  </HomeStack.Navigator>
+)
+const ProfileStackScreen=()=>(
+  <ProfileStack.Navigator>
+  <ProfileStack.Screen name="Profile" component={ProfileScreen}/>
+  </ProfileStack.Navigator>
+)
+const TimetableStackScreen=()=>(
+  <TimetableStack.Navigator>
+  <TimetableStack.Screen name="Time-Table" component={TimeTable}/>
+  </TimetableStack.Navigator>
+)
+export default function MainPage() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+       tabBarOptions={{
+        activeTintColor: 'dodgerblue',
+        style: {
+          backgroundColor: 'white'
+        }
+      }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'ios-home';
+            } else if (route.name === 'Profile') {
+              iconName = 'ios-person';
+            } else if (route.name === 'Time-table') {
+              iconName = 'ios-list-box';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}>
+        <Tab.Screen name="Time-table" component={TimetableStackScreen}  />
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Profile" component={ProfileStackScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
