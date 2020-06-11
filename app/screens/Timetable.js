9c710abfe10456bb2a5c2e98147f86d635feda62
Ayush Pattnaik <53592579+ayushpattnaik@users.noreply.github.com>
2020-06-11 21:12:32 +0530

    Add files via upload

---
+++ app/screens/Timetable.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+function TimeTable() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Under Construction</Text>
+      </View>
+    );
+  }
+  export default TimeTable;
