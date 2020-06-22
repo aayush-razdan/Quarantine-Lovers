dfcb11e4082dacfd2f5b0c3adeb452af3b7e1346
Aayush Razdan <razdanaayush19@gmail.com>
2020-06-22 17:41:32 +0530

    structured the redux app

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import * as React from "react";
 import DrawerStack from "./app/routes/Drawer";
+import store from "./app/src/store";
+import { Provider } from "react-redux";
 
 export default function App() {
-  return <DrawerStack />;
+  return (
+    <Provider store={store}>
+      <DrawerStack />
+    </Provider>
+  );
 }
