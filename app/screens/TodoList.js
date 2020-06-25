73928e204535d770fa7f61653cd2619b54956043
Ayush Pattnaik <pattnaikayush21@gmail.com>
2020-06-26 01:31:44 +0530

    todo app updated

--- app/screens/TodoList.js
+++ app/screens/TodoList.js
@@ -4,7 +4,7 @@ import {
   View,
   StyleSheet,
   TextInput,
-  Picker,
+ Picker,
   FlatList,
   Image,
   TouchableOpacity,
@@ -12,7 +12,6 @@ import {
   Modal,
 } from 'react-native';
 import DatePicker from 'react-native-datepicker';
-import { robotoWeights, material } from 'react-native-typography';
 import { createStackNavigator } from '@react-navigation/stack';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -20,37 +19,37 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 var deviceWidth = Dimensions.get('window').width;
 var deviceHeight = Dimensions.get('window').height;
 var d = new Date();
-var randomColor = require('randomcolor');
-var color = randomColor();
+var randomColor=require('randomcolor');
+var color=randomColor();
 
 const GoalItem = (props) => (
   <View style={styles.listItem}>
-    <Text style={styles.titleHead}>{props.title}</Text>
-    <View style={styles.category}>
-      <Icon name="circle" color="#203557" />
-      <Text style={styles.outputCategory}>{props.category}</Text>
-    </View>
-    <Text style={{ marginLeft: "3%", marginTop: "1%", marginBottom: "3%" }}>{props.details}</Text>
+    <Text style={{fontWeight:'bold', marginTop:deviceHeight/95,fontSize:deviceHeight/35, marginLeft:"2%"}}>{props.title}</Text>
+   <View style={{flexDirection:"row",position:"absolute",alignSelf:"flex-end"}}>
+     <Icon name="circle" color="#203557"/>
+     <Text>{props.category}</Text>
+   </View>
+    <Text style={{marginLeft:"3%",marginTop:"1%", marginBottom: "3%"}}>{props.details}</Text>
 
-  </View>
+   </View>
 );
 
-function Todo() {
+ function Todo() {
   const [modalVisible, setModalVisible] = useState(false);
   const [enteredText, setEnteredText] = useState('');
   const [enteredDetails, setEnteredDetails] = useState('');
   const [date, setDate] = useState(d);
-  const [category, setCategory] = useState("College")
+  const[category, setCategory]=useState("College")
 
   const [courseGoals, changeCourseGoals] = useState([]);
   const addText = (text) => {
     setEnteredText(text);
   };
-
+  
   const addDetails = (text) => {
     setEnteredDetails(text);
   };
-
+ 
   const addGoal = () => {
     changeCourseGoals((currentGoals) => [
       ...courseGoals,
@@ -65,92 +64,93 @@ function Todo() {
   };
 
   return (
-    <View style={{ backgroundColor: 'white', height: deviceHeight }}>
-      <Image source={require("../../assets/todo.jpg")} style={styles.header} />
-      <Modal animationType="slide" visible={modalVisible}>
-        <Icon name="close-circle" size={deviceHeight / 25} color="rgba(162,161,139,0.4)" style={{ alignSelf: "flex-end", marginTop: 3 }} onPress={() => setModalVisible(!modalVisible)} />
-        <View style={styles.view}>
+    <View style={{ backgroundColor: '#fff', height: deviceHeight }}>
+      <Image source={{uri: 'https://cdn.wallpaperhub.app/cloudcache/7/4/f/3/d/5/74f3d51cbec9db78da32e103de1b28538af1b76a.jpg'}} style={styles.header}/>
+      <Modal  animationType="fade" transparent={true} visible={modalVisible}>
+          <View style={styles.view}>
+          <Icon name="close-circle" size={deviceHeight/25} color="rgba(162,161,139,0.4)" style={{alignSelf:"flex-end", marginTop:3}} onPress={()=>setModalVisible(!modalVisible)}/>
 
-          <TextInput
-            style={styles.textInput}
-            placeholder={'Title'}
-            onChangeText={addText}
-            value={enteredText}
-          />
-          <Text style={{ fontSize: 20, marginTop: deviceHeight / 4, marginBottom: 10 }}>
-            Schedule related
-            </Text>
-          <View style={{ flexDirection: 'row', marginBottom: 10 }}>
-            <Icon name="note-text" size={30} color="#EE481B" />
             <TextInput
-              style={{ marginLeft: 10, fontSize: 16, width: "80%" }}
-              placeholder={'Details'}
-              onChangeText={addDetails}
-              value={enteredDetails}
+              style={styles.textInput}
+              placeholder={'Title'}
+              onChangeText={addText}
+              value={enteredText}
             />
-          </View>
-          <DatePicker
-            style={{ width: '90%' }}
-            date={date}
-            mode="date"
-            placeholder="select date"
-            format="DD-MM-YYYY"
-            confirmBtnText="Confirm"
-            cancelBtnText="Cancel"
-            customStyles={{
-              dateIcon: {
-                position: 'absolute',
-                left: 0,
-                top: 4,
-                marginLeft: 0,
-              },
-              dateInput: {
-                width: deviceWidth / 2,
-                marginLeft: 36,
-              },
-              // ... You can check the source to find the other keys.
-            }}
-            onDateChange={(date) => setDate(date)}
-          />
-
-          <Text style={{ fontSize: 20, marginTop: 10 }}>Category</Text>
-          <Picker
-            selectedValue={category}
-            style={{ height: 50, width: 150 }}
-            onValueChange={(itemValue, itemIndex) => setCategory(itemValue)}
-          >
-            <Picker.Item label="Home" value="Home" />
-            <Picker.Item label="College" value="College" />
-          </Picker>
-          <TouchableOpacity
-            style={styles.button1}
-            onPress={() => {
-              addGoal();
-              setModalVisible(!modalVisible);
-
-            }}>
-            <View style={styles.addPosition}>
-              <Text style={styles.add}>ADD</Text>
+            <Text style={{ fontSize: 20, marginTop: 15}}>
+              Schedule related
+            </Text>
+            <View style={{ flexDirection: 'row', marginTop:4 }}>
+            <Icon name="note-text" size={30} color="#292e49"/>
+              <TextInput
+                style={{ marginLeft: 10, fontSize: 16, width:"80%"}}
+                placeholder={'Details'}
+                onChangeText={addDetails}
+                value={enteredDetails}
+              />
             </View>
-          </TouchableOpacity>
-        </View>
-
-      </Modal>
-      <View>
-        <FlatList
-          inverted
-          keyExtractor={(item, index) => item.id}
-          data={courseGoals}
-          renderItem={(itemData) => (
-            <GoalItem
-              title={itemData.item.title}
-              details={itemData.item.details}
-
-              category={itemData.item.category}
+            <DatePicker
+              style={{ width: '90%' }}
+              date={date}
+              mode="date"
+              placeholder="select date"
+              format="YYYY-MM-DD"
+              confirmBtnText="Confirm"
+              cancelBtnText="Cancel"
+              customStyles={{
+                dateIcon: {
+                  position: 'absolute',
+                  left: 0,
+                  top: 4,
+                  
+                  marginLeft: 0,
+                },
+                dateInput: {
+                  marginLeft: 36,
+                },
+                // ... You can check the source to find the other keys.
+              }}
+              onDateChange={(date) => setDate(date)}
             />
-          )} />
 
-      </View>
+            <Text style={{ fontSize: 20, marginTop: 20 }}>Category</Text>
+            <Picker
+        selectedValue={category}
+        style={{ height: 50, width: 150 }}
+        onValueChange={(itemValue, itemIndex) => setCategory(itemValue)}
+      >
+        <Picker.Item label="Home" value="Home" />
+        <Picker.Item label="College" value="College" />
+        <Picker.Item label="Hostel" value="Hostel"/>
+      </Picker>
+            <TouchableOpacity
+              style={styles.button1}
+              onPress={() => {
+                addGoal();
+                setModalVisible(!modalVisible);
+        
+                }}>
+              <View style={styles.addPosition}>
+                <Text style={styles.add}>ADD</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+   
+      </Modal>
+    <View style={{height:deviceHeight-deviceHeight/2.7}}>
+      <FlatList
+      inverted
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <GoalItem
+            title={itemData.item.title}
+            details={itemData.item.details}
+            
+            category={itemData.item.category}
+          />
+        )} />
+    
+</View>
       <View
         style={{
           marginTop: deviceHeight / 20,
@@ -160,6 +160,7 @@ function Todo() {
         <TouchableOpacity
           onPress={() => setModalVisible(!modalVisible)}
           style={styles.button}>
+          
           <Text style={styles.addButton}>
             +
           </Text>
@@ -169,17 +170,17 @@ function Todo() {
   );
 }
 
-const Stack = createStackNavigator();
+const Stack=createStackNavigator();
 
-export default function TodoList() {
-  return (
+export default function TodoList(){
+  return(
 
-    <Stack.Navigator>
-
-      <Stack.Screen name="TodoList" component={Todo} options={{ title: "TODO" }} />
-
-    </Stack.Navigator>
-
+  <Stack.Navigator>
+  
+  <Stack.Screen name="TodoList" component={Todo} options={{title:"TODO"}}/>
+ 
+  </Stack.Navigator>
+  
   );
 }
 
@@ -188,46 +189,54 @@ const styles = StyleSheet.create({
     padding: 0,
     marginLeft: deviceWidth / 50,
     width: '96%',
-    //height: 110,
-    //borderLeftColor: "#3A2C6F",
-    borderColor: "skyblue",
+   
+    borderLeftColor: "#3A2C6F",
+   
     marginTop: deviceHeight / 30,
     borderLeftWidth: 4,
     borderWidth: 0,
     backgroundColor: 'white',
     borderRadius: 4.7,
-    elevation: 8,
+    elevation: 5,
   },
   button: {
     shadowColor: 'rgba(0,0,0, .4)', // IOS
     shadowOffset: { height: 2, width: 1 }, // IOS
     shadowOpacity: 1, // IOS
     shadowRadius: 7, //IOS
-    backgroundColor: 'skyblue',
+    backgroundColor: '#24385B',
     elevation: 8, // Android
     borderWidth: 0,
     borderColor: 'white',
-    borderBottomColor: "skyblue",
     alignItems: 'center',
     justifyContent: 'center',
     width: 70,
-    opacity: 0.7,
     height: 70,
-    borderRadius: 50,
+    opacity:0.85,
     position: "absolute",
     top: deviceHeight - deviceHeight / 4,
-
+    borderRadius: 50,
+    
   },
   textInput: {
     width: '80%',
-    fontSize: 25,
+    fontSize: 30,
   },
   view: {
-    padding: 10,
+    width:deviceWidth/1.1,
+    marginLeft:"4.5%",
+    backgroundColor:"white",
+    opacity:0.95,
+    padding:10,
     flexDirection: 'column',
     justifyContent: 'space-between',
-    height: deviceHeight / 1.4,
-    marginTop: "0%"
+    height:deviceHeight/1.4,
+    marginTop:"35%",
+    
+    borderRadius:15,
+    elevation:10
+    
+   
   },
   button1: {
     shadowColor: 'rgba(0,0,0, .4)', // IOS
@@ -238,48 +247,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#292e49',
     elevation: 2, // Android
     height: 50,
-    marginTop: '60%',
+   marginTop:"42%"
   },
   add: {
     fontSize: 18,
     fontWeight: 'bold',
     color: 'white',
-
   },
   addPosition: {
     alignItems: 'center',
-    marginTop: 12
+    marginTop:12
   },
   header: {
-    height: deviceHeight / 3,
-    opacity: 0.5,
+    
+    height: deviceHeight / 3.5
   },
   addButton: {
     position: "absolute",
     bottom: deviceHeight / 130,
-    right: deviceWidth / 19.5,
+    right: deviceWidth / 18,
     fontSize: deviceHeight / 18,
     color: "white",
-    opacity: 0.5
+    opacity: 0.8
   },
-  titleHead: {
-    ...robotoWeights.medium,
-    marginTop: deviceHeight / 95,
-    fontSize: deviceHeight / 35,
-    marginLeft: "2%",
-  },
-  category: {
-    flexDirection: "row",
-    position: "absolute",
-    right: deviceWidth / 6,
-    top: deviceHeight / 90,
-    alignSelf: "flex-end"
-  },
-  outputCategory: {
-    flexDirection: "row",
-    position: "absolute",
-    right: - deviceWidth / 7,
-    top: - deviceHeight / 250,
-
-  }
 });
