1a83cdfd4597b6c0ffc23cd941b9e25db2416b24
Ayush Pattnaik <pattnaikayush21@gmail.com>
2020-06-29 00:04:41 +0530

    map

--- app/screens/CollegeMap.js
+++ app/screens/CollegeMap.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react';
-import {
-    View
-} from 'react-native'
-export default function CollegeMap() {
-    return (
-        <View></View>
-    );
-}
+import React, { useState } from "react";
+
+import MapView, {Marker} from "react-native-maps";
+
+const CollegeMap = () => {
+
+  return (
+    <MapView 
+      style={{flex:1}}
+        initialRegion={{
+        latitude: 19.0223415,
+        longitude: 72.8560332,
+        latitudeDelta: 0.0008,
+        longitudeDelta: 0.0008
+        }}
+    > 
+      <Marker coordinate={{ latitude: 19.0223415, longitude:72.8560332 }} />
+    </MapView>
+  );
+};
+
+export default CollegeMap;
