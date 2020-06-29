d3a6534bb0cdedf49074a00f69ac0d6da5b30854
Aayush Razdan <razdanaayush19@gmail.com>
2020-06-29 18:17:30 +0530

    previous changes

--- App.js
+++ App.js
@@ -5,13 +5,15 @@ import AsyncStorage from "@react-native-community/async-storage";
 import { Provider } from "react-redux";
 import { persistStore, persistReducer } from "redux-persist";
 import { createLogger } from "redux-logger";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import { PersistGate } from "redux-persist/es/integration/react";
 import rootReducer from "./app/src/reducers/index";
 
 const persistConfig = {
   key: "root",
   storage: AsyncStorage,
-  whitelist: ["yearReducer", "branchReducer", "semReducer"],
+  stateReconciler: autoMergeLevel2,
+  whitelist: ["yearReducer", "branchReducer", "semReducer", "gpaReducer"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
