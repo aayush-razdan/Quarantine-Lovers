921b573a428d546a6d5759558f64db15a0d4b9bb
Aayush Razdan <razdanaayush19@gmail.com>
2020-07-01 15:15:53 +0530

    added firebase

--- App.js
+++ App.js
@@ -1,26 +1,21 @@
 import * as React from "react";
 import DrawerStack from "./app/routes/Drawer";
-import { createStore, applyMiddleware } from "redux";
-import AsyncStorage from "@react-native-community/async-storage";
 import { Provider } from "react-redux";
-import { persistStore, persistReducer } from "redux-persist";
-import { createLogger } from "redux-logger";
-import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import { PersistGate } from "redux-persist/es/integration/react";
-import rootReducer from "./app/src/reducers/index";
+import { store, persistedStore } from "./app/src/store/index";
 
-const persistConfig = {
-  key: "root",
-  storage: AsyncStorage,
-  stateReconciler: autoMergeLevel2,
-  whitelist: ["yearReducer", "branchReducer", "semReducer", "gpaReducer"],
+import * as firebase from "firebase";
+var firebaseConfig = {
+  apiKey: "AIzaSyA6pJCz3NkPCp2pyh8he2oF3pn5NaM6Ovg",
+  authDomain: "quarantineloversexpo.firebaseapp.com",
+  databaseURL: "https://quarantineloversexpo.firebaseio.com",
+  projectId: "quarantineloversexpo",
+  storageBucket: "quarantineloversexpo.appspot.com",
+  messagingSenderId: "1039687482395",
+  appId: "1:1039687482395:web:d73bf6d7cd6e6fa8488cbe",
 };
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const store = createStore(persistedReducer, applyMiddleware(createLogger()));
-
-const persistedStore = persistStore(store);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
