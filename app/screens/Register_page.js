fa7a1e15dfe30cfb27a1b0fde138a3cd9699054e
Aayush Razdan <razdanaayush19@gmail.com>
2020-07-02 15:54:31 +0530

    tried to add authentication

--- app/screens/Register_page.js
+++ app/screens/Register_page.js
@@ -1,147 +1,228 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+// import {
+//   ImageBackground,
+//   StyleSheet,
+//   View,
+//   Image,
+//   Text,
+//   TouchableWithoutFeedback,
+//   TextInput,
+//   TouchableOpacity,
+//   Dimensions,
+// } from "react-native";
+// import { } from "react-native-gesture-handler";
+// import { Dropdown } from 'react-native-material-dropdown';
+// import { Icon } from "react-native-elements";
+
+// const { width: WIDTH } = Dimensions.get("window");
+
+// export default class Register_page extends React.Component {
+//   render() {
+//     let data = [{
+//       value: 'Computer Science',
+//     },
+//     {
+//       value: 'Electronics and telecommunications',
+//     },
+//     {
+//       value: 'Electronics',
+//     },
+//     {
+//       value: 'Electrical',
+//     },
+//     {
+//       value: 'Mechanical',
+//     },
+//     {
+//       value: 'Production',
+//     },
+//     ];
+//     return (
+//       <View
+//         style={styles.background}
+//       >
+//         <View style={styles.inputContainer}>
+//           <TextInput
+//             style={styles.input}
+//             placeholder={"First Name"}
+//             placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
+//             underlineColorAndroid="transparent"
+//           />
+//         </View>
+//         <View style={styles.inputContainer}>
+//           <TextInput
+//             style={styles.input}
+//             placeholder={"Surname"}
+//             placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
+//             underlineColorAndroid="transparent"
+//           />
+//         </View>
+//         <View style={styles.inputContainer}>
+//           <TextInput
+//             style={styles.input}
+//             placeholder={"Semester (1-8)"}
+//             placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
+//             underlineColorAndroid="transparent"
+//           />
+//         </View>
+//         <View style={styles.inputDropdown}>
+//           <Dropdown
+//             style={{ color: 'rgba(255,255,255,0.7)' }}
+//             baseColor="rgba(255, 255, 255, 1)"
+//             inputContainerStyle={{ borderBottomWidth: 0 }}
+//             placeholder={"Branch"}
+//             placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
+//             data={data}
+//           >
+//           </Dropdown>
+//         </View>
+//         <View style={styles.inputContainer}>
+//           <TextInput
+//             style={styles.input}
+//             placeholder={"Username (College ID)"}
+//             placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
+//             underlineColorAndroid="transparent"
+//           />
+//         </View>
+//         <TouchableOpacity style={styles.btnRegister}>
+//           <Text style={styles.text}>Finish</Text>
+//         </TouchableOpacity>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   background: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "white",
+//   },
+//   input: {
+//     width: 250,
+//     height: 45,
+//     borderRadius: 25,
+//     fontSize: 16,
+//     paddingLeft: 45,
+//     backgroundColor: "rgba(0,0,0,0.35)",
+//     color: "rgba(255,255,255,0.7)",
+//     marginHorizontal: 25,
+//     justifyContent: "flex-end",
+//   },
+//   inputDropdown: {
+//     borderBottomColor: "transparent",
+//     width: 250,
+//     height: 45,
+//     borderRadius: 25,
+//     fontSize: 16,
+//     paddingLeft: 45,
+//     backgroundColor: "rgba(0,0,0,0.35)",
+//     color: "rgba(255,255,255,0.7)",
+//     marginHorizontal: 25,
+//     justifyContent: "flex-end",
+//     marginBottom: 5
+//   },
+//   inputContainer: {
+//     marginTop: 5,
+//     alignItems: "center",
+//     marginBottom: 10,
+//   },
+//   btnRegister: {
+//     width: 250,
+//     height: 45,
+//     borderRadius: 25,
+//     fontSize: 16,
+//     backgroundColor: "dodgerblue",
+//     justifyContent: "center",
+//     //marginTop: 10,
+//     position: "absolute",
+//     bottom: 57,
+//   },
+//   text: {
+//     color: "rgba(255,255,255,0.7)",
+//     fontSize: 16,
+//     textAlign: "center",
+//   },
+// });
+
 import {
-  ImageBackground,
-  StyleSheet,
-  View,
-  Image,
+  Container,
+  Content,
+  Header,
+  Form,
+  Input,
+  Item,
+  Button,
+  Label,
   Text,
-  TouchableWithoutFeedback,
-  TextInput,
-  TouchableOpacity,
-  Dimensions,
-} from "react-native";
-import { } from "react-native-gesture-handler";
-import { Dropdown } from 'react-native-material-dropdown';
-import { Icon } from "react-native-elements";
+} from "native-base";
 
-const { width: WIDTH } = Dimensions.get("window");
+import * as firebase from "firebase";
 
-export default class Register_page extends React.Component {
-  render() {
-    let data = [{
-      value: 'Computer Science',
-    },
-    {
-      value: 'Electronics and telecommunications',
-    },
-    {
-      value: 'Electronics',
-    },
-    {
-      value: 'Electrical',
-    },
-    {
-      value: 'Mechanical',
-    },
-    {
-      value: 'Production',
-    },
-    ];
-    return (
-      <View
-        style={styles.background}
-      >
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder={"First Name"}
-            placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
-            underlineColorAndroid="transparent"
+export default function Register_page() {
+  const [stateAuth, setStateAuth] = useState({
+    email: "",
+    password: "",
+  });
+
+  const signUpUser = (email, password) => {
+    try {
+      if (stateAuth.password.length < 6) {
+        alert("Please enter atleast 6 characters");
+        return;
+      }
+      firebase.auth().createUserWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.log(error.toString());
+    }
+  };
+  return (
+    <Container style={styles.container}>
+      <Form>
+        <Item floatingLabel>
+          <Label>Email</Label>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            onChangeText={(email) => setStateAuth({ email })}
           />
-        </View>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder={"Surname"}
-            placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
-            underlineColorAndroid="transparent"
+        </Item>
+        <Item floatingLabel>
+          <Label>Password</Label>
+          <Input
+            secureTextEntry={true}
+            autoCorrect={false}
+            autoCapitalize="none"
+            onChangeText={(password) => setStateAuth({ password })}
           />
-        </View>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder={"Semester (1-8)"}
-            placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
-            underlineColorAndroid="transparent"
-          />
-        </View>
-        <View style={styles.inputDropdown}>
-          <Dropdown
-            style={{ color: 'rgba(255,255,255,0.7)' }}
-            baseColor="rgba(255, 255, 255, 1)"
-            inputContainerStyle={{ borderBottomWidth: 0 }}
-            placeholder={"Branch"}
-            placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
-            data={data}
-          >
-          </Dropdown>
-        </View>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder={"Username (College ID)"}
-            placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
-            underlineColorAndroid="transparent"
-          />
-        </View>
-        <TouchableOpacity style={styles.btnRegister}>
-          <Text style={styles.text}>Finish</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+        </Item>
+        <Button
+          style={styles.button}
+          full
+          rounded
+          primary
+          onPress={() => signUpUser(stateAuth.email, stateAuth.password)}
+        >
+          <Text style={styles.text}>SignUp</Text>
+        </Button>
+      </Form>
+    </Container>
+  );
 }
 
 const styles = StyleSheet.create({
-  background: {
+  container: {
     flex: 1,
+    backgroundColor: "#fff",
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
+    padding: 10,
   },
-  input: {
-    width: 250,
-    height: 45,
-    borderRadius: 25,
-    fontSize: 16,
-    paddingLeft: 45,
-    backgroundColor: "rgba(0,0,0,0.35)",
-    color: "rgba(255,255,255,0.7)",
-    marginHorizontal: 25,
-    justifyContent: "flex-end",
-  },
-  inputDropdown: {
-    borderBottomColor: "transparent",
-    width: 250,
-    height: 45,
-    borderRadius: 25,
-    fontSize: 16,
-    paddingLeft: 45,
-    backgroundColor: "rgba(0,0,0,0.35)",
-    color: "rgba(255,255,255,0.7)",
-    marginHorizontal: 25,
-    justifyContent: "flex-end",
-    marginBottom: 5
-  },
-  inputContainer: {
-    marginTop: 5,
-    alignItems: "center",
-    marginBottom: 10,
-  },
-  btnRegister: {
-    width: 250,
-    height: 45,
-    borderRadius: 25,
-    fontSize: 16,
-    backgroundColor: "dodgerblue",
-    justifyContent: "center",
-    //marginTop: 10,
-    position: "absolute",
-    bottom: 57,
+  button: {
+    marginTop: 10,
   },
   text: {
-    color: "rgba(255,255,255,0.7)",
-    fontSize: 16,
-    textAlign: "center",
+    color: "white",
   },
 });
--- app/screens/WelcomePage.js
+++ app/screens/WelcomePage.js
@@ -1,141 +1,227 @@
-import React from "react";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+// import {
+//   ImageBackground,
+//   StyleSheet,
+//   View,
+//   Image,
+//   Text,
+//   TouchableWithoutFeedback,
+//   TextInput,
+//   Dimensions,
+//   AppRegistry,
+// } from "react-native";
+// import { Formik } from "formik";
+// import { TouchableOpacity } from "react-native-gesture-handler";
+// //import { Icon } from "react-native-vector-icons/Ionicons";
+// import { Button } from "react-native-elements";
+// import Authentication from "../other/authentication";
+
+// const { width: WIDTH } = Dimensions.get("window");
+
+// export default function WelcomePage({ navigation }) {
+//   return (
+//     <View style={styles.background}>
+//       <Formik
+//         initialValues={{ username: "", password: "" }}
+//         onSubmit={(values) => {
+//           console.log(values);
+//           //Authentication(values);
+//         }}
+//       >
+//         {(props) => (
+//           <View>
+//             <View style={styles.inputContainer}>
+//               <TextInput
+//                 style={styles.input}
+//                 placeholder={"Username"}
+//                 onChangeText={props.handleChange("username")}
+//                 value={props.values.title}
+//                 placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
+//                 underlineColorAndroid="transparent"
+//               />
+//             </View>
+//             <View style={styles.inputContainer}>
+//               <TextInput
+//                 style={styles.input}
+//                 placeholder={"Password"}
+//                 secureTextEntry={true}
+//                 onChangeText={props.handleChange("password")}
+//                 value={props.values.password}
+//                 placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
+//                 underlineColorAndroid="transparent"
+//               />
+//               {/* <TouchableOpacity style={styles.btnEye}>
+//                 <Text style={{ color: "rgba(0,0,0,0.35)" }}>^</Text>
+//               </TouchableOpacity> */}
+//               <TouchableOpacity
+//                 style={styles.btnLogin}
+//                 onPress={props.handleSubmit}
+//               >
+//                 <Text style={styles.text}>Login</Text>
+//               </TouchableOpacity>
+//               {/* <TouchableOpacity
+//                 style={styles.btnRegister}
+//                 onPress={() => props.navigation.navigate("Register")}
+//                 title="Register"
+//               >
+//                 <Text style={styles.text}>Register</Text>
+//               </TouchableOpacity> */}
+//             </View>
+//           </View>
+//         )}
+//       </Formik>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   background: {
+//     flex: 1,
+//     backgroundColor: "white",
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+//   input: {
+//     width: 250,
+//     height: 45,
+//     borderRadius: 25,
+//     fontSize: 16,
+//     paddingLeft: 45,
+//     backgroundColor: "rgba(0,0,0,0.35)",
+//     color: "rgba(255,255,255,0.7)",
+//     marginHorizontal: 25,
+//     justifyContent: "flex-end",
+//   },
+//   inputIcon: {
+//     position: "absolute",
+//     top: 8,
+//     left: 37,
+//   },
+//   inputContainer: {
+//     marginTop: 5,
+//     alignItems: "center",
+//     marginBottom: 10,
+//   },
+//   motto: {
+//     position: "absolute",
+//     top: 87,
+//     alignItems: "center",
+//   },
+//   btnEye: {
+//     position: "absolute",
+//     top: 10,
+//     right: 37,
+//   },
+//   btnLogin: {
+//     width: 250,
+//     height: 45,
+//     borderRadius: 25,
+//     fontSize: 16,
+//     backgroundColor: "dodgerblue",
+//     justifyContent: "center",
+//     marginTop: 50,
+//     marginBottom: 50,
+//   },
+//   btnRegister: {
+//     width: 250,
+//     height: 45,
+//     borderRadius: 25,
+//     fontSize: 16,
+//     backgroundColor: "dodgerblue",
+//     justifyContent: "center",
+//     marginTop: 10,
+//     position: "absolute",
+//     bottom: 5,
+//   },
+//   text: {
+//     color: "rgba(255,255,255,0.7)",
+//     fontSize: 16,
+//     textAlign: "center",
+//   },
+// });
+
 import {
-  ImageBackground,
-  StyleSheet,
-  View,
-  Image,
+  Container,
+  Content,
+  Header,
+  Form,
+  Input,
+  Item,
+  Button,
+  Label,
   Text,
-  TouchableWithoutFeedback,
-  TextInput,
-  Dimensions,
-  AppRegistry,
-} from "react-native";
-import { Formik } from "formik";
-import { TouchableOpacity } from "react-native-gesture-handler";
-//import { Icon } from "react-native-vector-icons/Ionicons";
-import { Button } from "react-native-elements";
-import Authentication from "../other/authentication";
+} from "native-base";
 
-const { width: WIDTH } = Dimensions.get("window");
+import * as firebase from "firebase";
 
-export default function WelcomePage({ navigation }) {
+export default function Register_page() {
+  const [stateAuth, setStateAuth] = useState({
+    email: "",
+    password: "",
+  });
+
+  const loginUser = (email, password) => {
+    try {
+      if (stateAuth.password.length < 6) {
+        alert("Please enter atleast 6 characters");
+        return;
+      }
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .then(function (user) {
+          console.log(user);
+        });
+    } catch (error) {
+      console.log(error.toString());
+    }
+  };
   return (
-    <View style={styles.background}>
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values);
-          //Authentication(values);
-        }}
-      >
-        {(props) => (
-          <View>
-            <View style={styles.inputContainer}>
-              <TextInput
-                style={styles.input}
-                placeholder={"Username"}
-                onChangeText={props.handleChange("username")}
-                value={props.values.title}
-                placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
-                underlineColorAndroid="transparent"
-              />
-            </View>
-            <View style={styles.inputContainer}>
-              <TextInput
-                style={styles.input}
-                placeholder={"Password"}
-                secureTextEntry={true}
-                onChangeText={props.handleChange("password")}
-                value={props.values.password}
-                placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
-                underlineColorAndroid="transparent"
-              />
-              {/* <TouchableOpacity style={styles.btnEye}>
-                <Text style={{ color: "rgba(0,0,0,0.35)" }}>^</Text>
-              </TouchableOpacity> */}
-              <TouchableOpacity
-                style={styles.btnLogin}
-                onPress={props.handleSubmit}
-              >
-                <Text style={styles.text}>Login</Text>
-              </TouchableOpacity>
-              {/* <TouchableOpacity
-                style={styles.btnRegister}
-                onPress={() => props.navigation.navigate("Register")}
-                title="Register"
-              >
-                <Text style={styles.text}>Register</Text>
-              </TouchableOpacity> */}
-            </View>
-          </View>
-        )}
-      </Formik>
-    </View>
+    <Container style={styles.container}>
+      <Form>
+        <Item floatingLabel>
+          <Label>Email</Label>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            onChangeText={(email) => setStateAuth({ email })}
+          />
+        </Item>
+        <Item floatingLabel>
+          <Label>Password</Label>
+          <Input
+            secureTextEntry={true}
+            autoCorrect={false}
+            autoCapitalize="none"
+            onChangeText={(password) => setStateAuth({ password })}
+          />
+        </Item>
+        <Button
+          style={styles.button}
+          full
+          rounded
+          primary
+          onPress={() => loginUser(stateAuth.email, stateAuth.password)}
+        >
+          <Text style={styles.text}>Login</Text>
+        </Button>
+      </Form>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
+  container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#fff",
     justifyContent: "center",
-    alignItems: "center",
+    padding: 10,
   },
-  input: {
-    width: 250,
-    height: 45,
-    borderRadius: 25,
-    fontSize: 16,
-    paddingLeft: 45,
-    backgroundColor: "rgba(0,0,0,0.35)",
-    color: "rgba(255,255,255,0.7)",
-    marginHorizontal: 25,
-    justifyContent: "flex-end",
-  },
-  inputIcon: {
-    position: "absolute",
-    top: 8,
-    left: 37,
-  },
-  inputContainer: {
-    marginTop: 5,
-    alignItems: "center",
-    marginBottom: 10,
-  },
-  motto: {
-    position: "absolute",
-    top: 87,
-    alignItems: "center",
-  },
-  btnEye: {
-    position: "absolute",
-    top: 10,
-    right: 37,
-  },
-  btnLogin: {
-    width: 250,
-    height: 45,
-    borderRadius: 25,
-    fontSize: 16,
-    backgroundColor: "dodgerblue",
-    justifyContent: "center",
-    marginTop: 50,
-    marginBottom: 50,
-  },
-  btnRegister: {
-    width: 250,
-    height: 45,
-    borderRadius: 25,
-    fontSize: 16,
-    backgroundColor: "dodgerblue",
-    justifyContent: "center",
+  button: {
     marginTop: 10,
-    position: "absolute",
-    bottom: 5,
   },
   text: {
-    color: "rgba(255,255,255,0.7)",
-    fontSize: 16,
-    textAlign: "center",
+    color: "white",
   },
 });
