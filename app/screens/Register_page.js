0cd6f90abc5e6fc39bf49c5885b325a9a657a318
Aayush Razdan <razdanaayush19@gmail.com>
2020-07-05 21:36:27 +0530

    added other inputs in signup page now the ui of dropdown is to be done'

--- app/screens/Register_page.js
+++ app/screens/Register_page.js
@@ -1,151 +1,5 @@
 import React, { useState } from "react";
 import { StyleSheet } from "react-native";
-// import {
-//   ImageBackground,
-//   StyleSheet,
-//   View,
-//   Image,
-//   Text,
-//   TouchableWithoutFeedback,
-//   TextInput,
-//   TouchableOpacity,
-//   Dimensions,
-// } from "react-native";
-// import { } from "react-native-gesture-handler";
-// import { Dropdown } from 'react-native-material-dropdown';
-// import { Icon } from "react-native-elements";
-
-// const { width: WIDTH } = Dimensions.get("window");
-
-// export default class Register_page extends React.Component {
-//   render() {
-//     let data = [{
-//       value: 'Computer Science',
-//     },
-//     {
-//       value: 'Electronics and telecommunications',
-//     },
-//     {
-//       value: 'Electronics',
-//     },
-//     {
-//       value: 'Electrical',
-//     },
-//     {
-//       value: 'Mechanical',
-//     },
-//     {
-//       value: 'Production',
-//     },
-//     ];
-//     return (
-//       <View
-//         style={styles.background}
-//       >
-//         <View style={styles.inputContainer}>
-//           <TextInput
-//             style={styles.input}
-//             placeholder={"First Name"}
-//             placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
-//             underlineColorAndroid="transparent"
-//           />
-//         </View>
-//         <View style={styles.inputContainer}>
-//           <TextInput
-//             style={styles.input}
-//             placeholder={"Surname"}
-//             placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
-//             underlineColorAndroid="transparent"
-//           />
-//         </View>
-//         <View style={styles.inputContainer}>
-//           <TextInput
-//             style={styles.input}
-//             placeholder={"Semester (1-8)"}
-//             placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
-//             underlineColorAndroid="transparent"
-//           />
-//         </View>
-//         <View style={styles.inputDropdown}>
-//           <Dropdown
-//             style={{ color: 'rgba(255,255,255,0.7)' }}
-//             baseColor="rgba(255, 255, 255, 1)"
-//             inputContainerStyle={{ borderBottomWidth: 0 }}
-//             placeholder={"Branch"}
-//             placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
-//             data={data}
-//           >
-//           </Dropdown>
-//         </View>
-//         <View style={styles.inputContainer}>
-//           <TextInput
-//             style={styles.input}
-//             placeholder={"Username (College ID)"}
-//             placeholderTextColorTextColor={"rgba(255,255,255,0.7)"}
-//             underlineColorAndroid="transparent"
-//           />
-//         </View>
-//         <TouchableOpacity style={styles.btnRegister}>
-//           <Text style={styles.text}>Finish</Text>
-//         </TouchableOpacity>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   background: {
-//     flex: 1,
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: "white",
-//   },
-//   input: {
-//     width: 250,
-//     height: 45,
-//     borderRadius: 25,
-//     fontSize: 16,
-//     paddingLeft: 45,
-//     backgroundColor: "rgba(0,0,0,0.35)",
-//     color: "rgba(255,255,255,0.7)",
-//     marginHorizontal: 25,
-//     justifyContent: "flex-end",
-//   },
-//   inputDropdown: {
-//     borderBottomColor: "transparent",
-//     width: 250,
-//     height: 45,
-//     borderRadius: 25,
-//     fontSize: 16,
-//     paddingLeft: 45,
-//     backgroundColor: "rgba(0,0,0,0.35)",
-//     color: "rgba(255,255,255,0.7)",
-//     marginHorizontal: 25,
-//     justifyContent: "flex-end",
-//     marginBottom: 5
-//   },
-//   inputContainer: {
-//     marginTop: 5,
-//     alignItems: "center",
-//     marginBottom: 10,
-//   },
-//   btnRegister: {
-//     width: 250,
-//     height: 45,
-//     borderRadius: 25,
-//     fontSize: 16,
-//     backgroundColor: "dodgerblue",
-//     justifyContent: "center",
-//     //marginTop: 10,
-//     position: "absolute",
-//     bottom: 57,
-//   },
-//   text: {
-//     color: "rgba(255,255,255,0.7)",
-//     fontSize: 16,
-//     textAlign: "center",
-//   },
-// });
 
 import {
   Container,
@@ -158,6 +12,7 @@ import {
   Label,
   Text,
 } from "native-base";
+import DropDownPicker from "react-native-dropdown-picker";
 
 import { Firebase, db } from "../firebase/firebase";
 
@@ -171,8 +26,14 @@ export default function Register_page() {
   const [Name, setName] = useState({
     name: "",
   });
+  const [Semester, setSemester] = useState({
+    semester: null,
+  });
+  const [Branch, setBranch] = useState({
+    branch: null,
+  });
 
-  const signUpUser = (email, password, name) => {
+  const signUpUser = (email, password, name, sem, branch) => {
     try {
       if (Password.password.length < 6) {
         alert("Please enter atleast 6 characters");
@@ -183,6 +44,8 @@ export default function Register_page() {
         .then((cred) => {
           return db.collection("users").doc(cred.user.uid).set({
             name: name,
+            semester: sem,
+            branch: branch,
           });
         })
         .then(() => {
@@ -196,7 +59,7 @@ export default function Register_page() {
     <Container style={styles.container}>
       <Form>
         <Item floatingLabel>
-          <Label>Name</Label>
+          <Label>Full Name</Label>
           <Input
             autoCorrect={false}
             autoCapitalize="none"
@@ -220,13 +83,88 @@ export default function Register_page() {
             onChangeText={(password) => setPassword({ password })}
           />
         </Item>
+
+        <DropDownPicker
+          items={[
+            {
+              label: "1",
+              value: "1",
+            },
+            {
+              label: "2",
+              value: "2",
+            },
+          ]}
+          defaultValue={Semester.semester}
+          placeholder="Semester"
+          containerStyle={{ height: 40, marginTop: 20 }}
+          style={{ backgroundColor: "#fafafa" }}
+          itemStyle={{
+            justifyContent: "flex-start",
+          }}
+          dropDownStyle={{ backgroundColor: "#fafafa" }}
+          onChangeItem={(item) =>
+            setSemester({
+              semester: item.value,
+            })
+          }
+        />
+
+        <DropDownPicker
+          items={[
+            {
+              label: "Batch 1",
+              value: "1",
+            },
+            {
+              label: "Batch 2",
+              value: "2",
+            },
+            {
+              label: "Batch 3",
+              value: "3",
+            },
+            {
+              label: "Batch 4",
+              value: "4",
+            },
+            {
+              label: "Batch 5",
+              value: "5",
+            },
+            {
+              label: "Batch 6",
+              value: "6",
+            },
+          ]}
+          defaultValue={Branch.branch}
+          placeholder="Batch"
+          containerStyle={{ height: 40, marginTop: 90 }}
+          style={{ backgroundColor: "#fafafa" }}
+          itemStyle={{
+            justifyContent: "flex-start",
+          }}
+          dropDownStyle={{ backgroundColor: "#fafafa" }}
+          onChangeItem={(item) =>
+            setBranch({
+              branch: item.value,
+            })
+          }
+        />
+
         <Button
           style={styles.button}
           full
           rounded
           primary
           onPress={() => {
-            signUpUser(Email.email, Password.password, Name.name);
+            signUpUser(
+              Email.email,
+              Password.password,
+              Name.name,
+              Semester.semester,
+              Branch.branch
+            );
           }}
         >
           <Text style={styles.text}>SignUp</Text>
@@ -240,11 +178,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
+
     padding: 10,
   },
   button: {
-    marginTop: 10,
+    marginTop: 160,
   },
   text: {
     color: "white",
