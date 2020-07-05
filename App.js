94f1cbe83a78a7cffbed8d1242a752b46737430f
Aayush Razdan <razdanaayush19@gmail.com>
2020-07-05 19:11:58 +0530

    modifications to remove atob error

--- App.js
+++ App.js
@@ -3,6 +3,17 @@ import DrawerStack from "./app/routes/Drawer";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/es/integration/react";
 import { store, persistedStore } from "./app/src/store/index";
+import { decode, encode } from "base-64";
+
+if (!global.btoa) {
+  global.btoa = encode;
+}
+
+if (!global.atob) {
+  global.atob = decode;
+}
+
+//console.disableYellowBox = true;
 
 export default function App() {
   return (
